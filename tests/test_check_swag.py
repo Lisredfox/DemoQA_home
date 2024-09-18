49493763c89b731001c6dfc848364ce33e856466
Lisrdfox <marinagaleevaorg@gmail.com>
2024-09-18 13:42:15 +0300

    First Commit

---
+++ tests/test_check_swag.py
@@ -0,0 +1,24 @@
+from conftest import browser
+from pages.swag_labs import SwagLabs
+from pages.base_page import BasePage
+
+
+def test(browser):
+    icon = BasePage(browser)
+    icon_2 = SwagLabs(browser)
+    icon.visit()
+    assert icon_2.exist_icon()
+
+
+def test_2(browser):
+    icon = BasePage(browser)
+    icon_2 = SwagLabs(browser)
+    icon.visit()
+    assert icon_2.name_assert()
+
+
+def test_3(browser):
+   icon = BasePage(browser)
+   icon_2 = SwagLabs(browser)
+   icon.visit()
+   assert icon_2.password_assert()
